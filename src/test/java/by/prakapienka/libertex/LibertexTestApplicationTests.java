84e44c391c38142903fdde58b02b6d563fb6162b
p-prakapienka <prakapienka.work@gmail.com>
2020-05-05 02:16:59 +0300

    clean up

--- src/test/java/by/prakapienka/libertex/LibertexTestApplicationTests.java
+++ src/test/java/by/prakapienka/libertex/LibertexTestApplicationTests.java
@@ -3,30 +3,22 @@ package by.prakapienka.libertex;
 import by.prakapienka.libertex.controller.UserController;
 import by.prakapienka.libertex.entity.User;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 @SpringBootTest
 class LibertexTestApplicationTests {
-
-	private ExecutorService executor;
 
 	@Autowired
 	private UserController controller;
 
 	@BeforeEach
 	void init() {
-		executor = Executors.newFixedThreadPool(10);
-
 		controller.create(new User("User 1"));
 		controller.create(new User("User 2"));
 		controller.create(new User("User 3"));
